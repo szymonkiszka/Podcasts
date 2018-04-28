88a2ff6f5cf9cb7285d306babe25b8e1bd96d491
szymonkiszka <szymonkiszka@icloud.com>
2018-04-28 22:32:22 +0200

    Create tab bar

---
+++ Podcasts/MainTabBarController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabBarController.swift
+//  Podcasts
+//
+//  Created by Simon on 28.04.2018.
+//  Copyright © 2018 Simon. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        viewControllers = [
+            generateNavigationController(for: UIViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: rootViewController)
+        
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        
+        return navController
+    }
+    
+    
+    
+    
+    
+    
+    
+}
