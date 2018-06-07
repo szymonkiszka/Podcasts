2691520d45eccff1bcafdfcfd04c98899c17188f
szymonkiszka <szymonkiszka@icloud.com>
2018-06-07 16:26:28 +0200

    Add episode player dashboard

--- Podcasts/Model/Episode.swift
+++ Podcasts/Model/Episode.swift
@@ -13,13 +13,20 @@ struct Episode {
     let title: String
     let pubDate: Date
     let description: String
+    let author: String
+    let streamUrl: String
     
     var imageUrl: String?
     
     init(feedItem: RSSFeedItem) {
+        self.streamUrl = feedItem.enclosure?.attributes?.url ?? ""
+        
+        
+        
         self.title = feedItem.title ?? ""
         self.pubDate = feedItem.pubDate ?? Date()
         self.description = feedItem.iTunes?.iTunesSubtitle ?? feedItem.description ?? ""
+        self.author = feedItem.iTunes?.iTunesAuthor ?? ""
         
         self.imageUrl = feedItem.iTunes?.iTunesImage?.attributes?.href
     }
--- Podcasts/View/PlayerDetailsView.swift
+++ Podcasts/View/PlayerDetailsView.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailsView: UIView {
     
     var episode: Episode! {
         didSet {
             titleLabel.text = episode.title
+            authorLabel.text = episode.author
+            
+            playEpisode()
             
             guard let url = URL(string: episode.imageUrl ?? "") else { return }
             
@@ -20,10 +24,52 @@ class PlayerDetailsView: UIView {
         }
     }
     
+    fileprivate func playEpisode() {
+        print("Trying to play episode at url:", episode.streamUrl)
+        
+        guard let url = URL(string: episode.streamUrl) else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
+    //MARK:= IB Actions and Outlets
+    
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
     }
     
+    @IBOutlet weak var playPauseButton: UIButton! {
+        didSet {
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+        }
+    }
+    
+    @objc func handlePlayPause() {
+        
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
+        
+    }
+    
+    
     @IBOutlet weak var episodeImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.numberOfLines = 2
+        }
+    }
 }
